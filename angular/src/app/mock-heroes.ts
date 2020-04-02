faf968ef0c7159bc0f13df377885b3324cd7bd7b
Stephen <archaether@gmail.com>
2020-04-02 14:45:52 -0400

    Display a List

---
+++ angular/src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 1, name: 'Bomber'},
+    {id: 2, name: 'Deku Shrub'},
+    {id: 3, name: 'Invisi-girl'},
+    {id: 4, name: 'Dark Souls'},
+    {id: 5, name: 'Jet Legs'}
+]; 
